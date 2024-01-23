92be77e9361a83acbb9428d9061c0056a68870db
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-01-23 15:49:47 +1300

    Initialize

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
@@ -0,0 +1,103 @@
+﻿import React from 'react';
+import Cookies from 'js-cookie';
+import { Card, Button, Icon, Label, Popup, ButtonGroup, CardGroup, Grid } from 'semantic-ui-react';
+import moment from 'moment';
+
+export class JobSummaryCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isPopupOpen: false
+        };
+        this.selectJob = this.selectJob.bind(this);
+        this.togglePopup = this.togglePopup.bind(this);
+    }
+
+    selectJob(id) {
+        console.log(id);
+        var cookies = Cookies.get('talentAuthToken');
+        var link = 'http://localhost:51689/listing/listing/closeJob';
+
+        $.ajax({
+            url: link,
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(id),
+            success: function (res) {
+                if (res.success == true) {
+                    TalentUtil.notification.show(res.message, "success", null, null);
+                    window.location = "/ManageJobs";
+                } else {
+                    TalentUtil.notification.show(res.message, "error", null, null)
+                }
+            }.bind(this)
+        })
+    }
+
+    togglePopup() {
+        this.setState(prevState => ({
+            isPopupOpen: !prevState.isPopupOpen
+        }));
+    }
+
+    render() {
+        const { job } = this.props; // Assuming job data is passed as a prop
+        const { id, title, location, summary, expiryDate, noOfSuggestions } = job;
+
+        const expiredLabel = expiryDate && moment(expiryDate).isBefore(moment()) ? (
+            <Label color='red'>Expired</Label>
+        ) : <Label className="placeholder-label job-summary">Expired</Label>;
+
+        return (
+            <Card className="card-job-summary">
+                <Card.Content>
+                    <Label as='a' color='black' ribbon='right'>
+                        <Icon name='user' /> {noOfSuggestions || 0}
+                    </Label>
+                    <Card.Header>{title}</Card.Header>
+                    <Card.Meta>
+                        {location ? `${location.city}, ${location.country}` : 'No location'}
+                    </Card.Meta>
+                    <Card.Description className="description job-summary">{summary}</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    {expiredLabel}
+                    <ButtonGroup size='mini' floated='right'>
+                        <Popup
+                            trigger={<Button icon="close" content="Close" basic color='blue' />}
+                            header="Would you like to close this job?"
+                            content={
+                                <div>
+                                    <Button
+                                        className="ui button job-summary"
+                                        onClick={() => this.selectJob(id)}
+                                        color='green'
+                                        size='mini'
+                                        floated="right">Yes</Button>
+                                    <Button
+                                        onClick={this.togglePopup}
+                                        color='red'
+                                        size='mini'
+                                        floated="right">No</Button>
+                                </div>
+                            }
+                            open={this.state.isPopupOpen}
+                            onOpen={this.togglePopup}
+                            onClose={this.togglePopup}
+                            on='click'
+                            position='top left'
+                        >
+                        </Popup>
+                        <Button icon="edit outline" content="Edit" basic color='blue' />
+                        <Button icon="copy outline" content="Copy" basic color='blue' />
+                    </ButtonGroup>
+                </Card.Content>
+            </Card>
+        );
+    }
+}
---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -0,0 +1,200 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Cookies from 'js-cookie';
+import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
+import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
+import { JobSummaryCard } from './JobSummaryCard.jsx';
+import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
+import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment, Card} from 'semantic-ui-react';
+
+export default class ManageJob extends React.Component {
+    constructor(props) {
+        super(props);
+        let loader = loaderData
+        loader.allowedUsers.push("Employer");
+        loader.allowedUsers.push("Recruiter");
+        //console.log(loader)
+        this.state = {
+            loadJobs: [],
+            loaderData: loader,
+            activePage: 1,
+            sortBy: {
+                date: "desc"
+            },
+            sortText: "Newest first",
+            filter: {
+                showActive: true,
+                showClosed: false,
+                showDraft: true,
+                showExpired: true,
+                showUnexpired: true
+            },
+            totalPages: 1,
+            activeIndex: ""
+        }
+        this.loadData = this.loadData.bind(this);
+        this.init = this.init.bind(this);
+        this.loadNewData = this.loadNewData.bind(this);
+        //your functions go here
+        this.handleItemClick = this.handleItemClick.bind(this);
+        this.handleSortChange = this.handleSortChange.bind(this);
+        this.handlePaginationChange = this.handlePaginationChange.bind(this);
+    };
+
+    init() {
+        let loaderData = TalentUtil.deepCopy(this.state.loaderData)
+        loaderData.isLoading = false;
+        //this.setState({ loaderData });//comment this
+
+        //set loaderData.isLoading to false after getting data
+        this.loadData(() =>
+            this.setState({ loaderData })
+        )
+        //console.log(this.state.loaderData)
+    }
+
+    componentDidMount() {
+        this.init();
+    };
+
+    loadData(callback) {
+        var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs';
+        var cookies = Cookies.get('talentAuthToken');
+        const { activePage, sortBy, filter } = this.state;
+        var params = new URLSearchParams({
+            activePage: activePage,
+            sortbyDate: sortBy.date,
+            showActive: filter.showActive,
+            showClosed: filter.showClosed,
+            showDraft: filter.showDraft,
+            showExpired: filter.showExpired,
+            showUnexpired: filter.showUnexpired
+        });
+       // your ajax call and other logic goes here
+        $.ajax({
+            url: link + '?' + params.toString(),
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            contentType: "application/json",
+            dataType: "json",
+            success: function (res) {
+                if (res.success == true) {
+                    this.setState({
+                        loadJobs: res.myJobs,
+                        totalPages: Math.ceil(res.totalCount / 6)
+                    })
+                } else {
+                    TalentUtil.notification.show(res.message, "error", null, null)
+                }
+                if (callback) {
+                    callback();
+                }
+            }.bind(this)
+        })
+    }
+
+    loadNewData(data) {
+        var loader = this.state.loaderData;
+        loader.isLoading = true;
+        data[loaderData] = loader;
+        this.setState(data, () => {
+            this.loadData(() => {
+                loader.isLoading = false;
+                this.setState({
+                    loadData: loader
+                })
+            })
+        });
+    }
+
+    handleItemClick(key) {
+        this.setState(prevState => ({
+            filter: Object.assign({}, prevState.filter, { [key]: !prevState.filter[key] }),
+            activePage: 1
+        }), () => {
+            this.loadData();
+        });
+    }
+
+    handleSortChange(direction) {
+        //  Update the sorting direction in the state
+        const sortText = direction === 'desc' ? 'Newest first' : 'Oldest first';
+        this.setState({ sortBy: { date: direction }, sortText }, () => {
+            // Fetch data after status update
+            this.loadData();
+        });
+    }
+
+    handlePaginationChange(e, { activePage }) {
+        this.setState({ activePage }, () => {
+            this.loadNewData({ activePage });
+        });
+    };
+
+    render() {
+        return (
+            <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
+                <section className="page-body">
+                    <div className="ui container">
+                        <div className="ui grid">
+                            <div className="row">
+                                <h1>List of Jobs</h1>
+                            </div>
+                            <div className="row">
+                                <Icon name="filter" /> <span>Filter: </span>
+                                <Dropdown className="ui dropdown manage-jobs" text='Choose Filter' simple item inline>
+                                    <Dropdown.Menu>
+                                        {Object.keys(this.state.filter).map((key) => (
+                                            <Dropdown.Item key={key} onClick={() => this.handleItemClick(key)}>
+                                                <Checkbox
+                                                    label={"Show " + key.slice(4)}
+                                                    name={key}
+                                                    checked={this.state.filter[key]}
+                                                />
+                                            </Dropdown.Item>
+                                        ))}
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                                <Icon name="calendar alternate outline" /> <span>Sort by date: </span>
+                                <Dropdown className="ui dropdown manage-jobs" text={this.state.sortText} simple item inline>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item text='Newest first' onClick={() => this.handleSortChange('desc')} />
+                                        <Dropdown.Item text='Oldest first' onClick={() => this.handleSortChange('asc')} />
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </div>
+                            <div className="row">
+                                {this.state.loadJobs.length > 0 ? (<Card.Group itemsPerRow={3}>
+                                    {this.state.loadJobs.map(job => (
+                                        <JobSummaryCard key={job.id} job={job} />
+                                    ))}
+                                </Card.Group>) : (<span>No Jobs Found</span>)}
+                            </div>
+                            <div className="row">
+                                <div className="sixteen wide center aligned padded column">
+                                    <Pagination
+                                        activePage={this.state.activePage}
+                                        onPageChange={this.handlePaginationChange}
+                                        totalPages={this.state.totalPages}
+                                        ellipsisItem={null}
+                                        firstItem={null}
+                                        lastItem={null}
+                                        ellipsisItem={{ content: <Icon name='ellipsis horizontal' />, icon: true }}
+                                        firstItem={{ content: <Icon name='angle double left' />, icon: true }}
+                                        lastItem={{ content: <Icon name='angle double right' />, icon: true }}
+                                        prevItem={{ content: <Icon name='angle left' />, icon: true }}
+                                        nextItem={{ content: <Icon name='angle right' />, icon: true }}
+                                    />
+                                </div>
+                            </div>
+                            <div className="row"></div>
+                        </div>
+                    </div>
+                </section>
+            </BodyWrapper>
+        )
+    }
+}
